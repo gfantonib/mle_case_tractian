5d8b86b7e3cf90547cfbc50f8703a036ca4e4ded
gfantoni <guilhermefantoniborges@gmail.com>
2024-08-21 16:43:29 -0300

    small output corrections on request script

--- api/request_script.py
+++ api/request_script.py
@@ -17,29 +17,42 @@ fit_url = "http://127.0.0.1:5000/{sensor_id}/fit"
 weights_url = "http://127.0.0.1:5000/{sensor_id}/weights"
 predict_url = "http://127.0.0.1:5000/{sensor_id}/predict"
 
-# Define iter max
 iter_max = 20
 
-# Function to generate random float values
 def generate_random_values(size):
-    return np.round(np.random.uniform(low=0.0, high=20.0, size=size), 2).tolist()
+    return np.round(np.random.uniform(low=0.0, high=15.0, size=size), 2).tolist()
 
-# Function to send POST request with random data
 def send_post_request(sensor_id):
-    values = generate_random_values(4)  # Adjust the size as needed
+    values = generate_random_values(4)
     data = {"values": values}
     response = requests.post(fit_url.format(sensor_id=sensor_id), json=data)
     return response
 
-# While loop to continuously send fit requests
 def fit_request_loop():
     i = 0
     while i < iter_max:
         sensor_id = random.choice(sensor_ids)
         response = send_post_request(sensor_id)
         print(f"Response for sensor ID {sensor_id}: {response.status_code} - {response.json()}")
-
         i += 1
+
+def weights_request_loop():
+    for sensor in sensor_ids:
+        response = requests.get(weights_url.format(sensor_id = sensor))
+        df = pd.DataFrame(response.json())
+        print(f"{df}\n")
+
+def predict_request_loop():
+    for sensor in sensor_ids:
+        values = generate_random_values(1)
+        data = {"values": values[0]}
+        response = requests.post(predict_url.format(sensor_id = sensor), json=data)
+        df_sensor_alert = pd.DataFrame({
+            'sensor_id': [sensor] * len(response.json()),
+            'alert': list(response.json().values())
+        })
+        print(f"{df_sensor_alert}\n")
+
 
 print("\n")
 print(f"MAKE {iter_max} FIT REQUESTS (POST):\n")
@@ -47,20 +60,9 @@ fit_request_loop()
 print("\n")
 
 print(f"MAKE {len(sensor_ids)} WEIGHTS REQUESTS (GET):\n")
-for sensor in sensor_ids:
-    response = requests.get(weights_url.format(sensor_id = sensor))
-    df = pd.DataFrame(response.json())
-    print(df)
+weights_request_loop()
 print("\n")
 
 print(f"MAKE {len(sensor_ids)} PREDICT REQUESTS (POST):\n")
-for sensor in sensor_ids:
-    values = generate_random_values(1)
-    data = {"values": values[0]}
-    response = requests.post(predict_url.format(sensor_id = sensor), json=data)
-    sensor_alert_df = pd.DataFrame({
-        'sensor_id': [sensor] * len(response.json()),
-        'alert': list(response.json())
-    })
-    print(sensor_alert_df)
+predict_request_loop()
 print("\n")
