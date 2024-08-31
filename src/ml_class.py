b9d26ce52e559f15ab960f0d80e70466ddeb1cd7
gfantoni <guilhermefantoniborges@gmail.com>
2024-08-31 09:58:39 -0300

    optmized model has no more responsability in preparing data: it assumes the data comes prepared.

--- src/ml_class.py
+++ src/ml_class.py
@@ -3,20 +3,15 @@ import pandas as pd
 class mlModel:
 
 	def __init__(self):
-		self.df = None
 		self.sensors_groups = None
 		self.df_sensors_groups_means = None
 		self.df_sensors_groups_stds = None
 		self.df_result = None
 
-	def prepare_data(self, df: pd.DataFrame):
-		self.df = df
-		df_light = self.df.drop(["_id", "createdAt", "params.duration", "params.sampRate", "params.timeStart", "temp"], axis=1)
-		self.sensors_groups = df_light.groupby("sensorId")
-
-	def train(self):
-		self.df_sensors_groups_means = self.sensors_groups.mean()
-		self.df_sensors_groups_stds = self.sensors_groups.std()
+	def train(self, df:pd.DataFrame):
+		sensors_groups = df.groupby("sensorId")
+		self.df_sensors_groups_means = sensors_groups.mean()
+		self.df_sensors_groups_stds = sensors_groups.std()
 
 	def predict(self, discriminator: float):
 		self.df_result = discriminator >= self.df_sensors_groups_means + 2*self.df_sensors_groups_stds
--- src/ml_run.py
+++ src/ml_run.py
@@ -3,12 +3,16 @@
 import pandas as pd
 from ml_class import mlModel
 
+def prepare_data(df:pd.DataFrame):
+	df_prepared = df.drop(["_id", "createdAt", "params.duration", "params.sampRate", "params.timeStart", "temp"], axis=1)
+	return df_prepared
+
 df = pd.read_csv("../data/collects.csv")
 new_value = 9e-2
+df_prepared = prepare_data(df)
 
 sensor_alert = mlModel()
-sensor_alert.prepare_data(df)
-sensor_alert.train()
+sensor_alert.train(df_prepared)
 sensor_alert.predict(new_value)
 
 print(sensor_alert.get_df_result())
